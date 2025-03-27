85a4e7fca8587e22b4e6af1f7041cb7559ba69fc
Yuliya Dabreha <yd000015@red.ujaen.es>
2025-03-27 14:24:59 +0100

    Created DB models in database.py

--- fastapi/database.py
+++ fastapi/database.py
@@ -12,15 +12,34 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-# Test the database connection
-try:
-    # Create a session
-    db = SessionLocal()
-    # Execute a simple query
-    db.execute(text("SELECT 1")).fetchone()
-    print("Database connection successful!")
-except Exception as e:
-    print(f"Database connection failed: {e}")
-finally:
-    # Ensure the session is closed
-    db.close()
+class CategoryDB(Base):
+    __tablename__ = "Category"
+    category_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(15), nullable=False)
+
+    trainings = relationship("TrainingDB", back_populates="category")  # Relationship to Training
+
+class TrainingDB(Base):
+    __tablename__ = "Training"
+    training_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("User.user_id"))
+    category_id = Column(Integer, ForeignKey("Category.category_id"))
+    date = Column(Date, nullable=False)
+    training_duration = Column(Integer, nullable=False)
+
+    user = relationship("UserDB", back_populates="trainings")  # Relationship to User
+    category = relationship("CategoryDB", back_populates="trainings")  # Relationship to Category
+
+class UserDB(Base):
+    __tablename__ = "User"
+    user_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    surname = Column(String(255), nullable=False)
+    phone_number = Column(String(15))
+    email = Column(String(255), unique=True, nullable=False)
+    address = Column(String(255))
+    password = Column(String(255), nullable=False)
+    subscription_type = Column(Enum("VIP", "premium", "standard"), nullable=False, default="standard")
+    role = Column(Enum("USER", "ADMIN"), nullable=False, default="ADMIN")
+
+    trainings = relationship("TrainingDB", back_populates="user")  # Relationship to Training
